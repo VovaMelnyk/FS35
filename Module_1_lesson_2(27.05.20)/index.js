99f125e95975f8bba6c647c41da9d624cfde64f4
Vova Melnyk <hagmen4339@gmail.com>
2021-05-27 22:14:05 +0300

    finish lesson 2

--- Module_1_lesson_2(27.05.20)/index.js
+++ Module_1_lesson_2(27.05.20)/index.js
@@ -1,3 +1,49 @@
+// console.log(0.1 + 0.2 === 0.3);
+// console.log(0.1 + 0.2);
+// let result = 0.1 + 0.2;
+// console.log(result)
+// console.log(Math.floor(result))
+// console.log(Math.ceil(result))
+// console.log(Math.round(result))
+// console.log(result.toFixed(2))
+
+// let name = 'Bob';
+// let slicedName = name.slice(0, 2);
+// console.log('name', name);
+// console.log("slicedName", slicedName)
+
+// function sum() {
+//     let result = null;
+//     result = 2 + 3;
+//     return result;
+// }
+
+// const myNewResult = result;
+
+// console.log("myNewResult", myNewResult)
+
+// console.log(0 || '' || null); // null
+
+// console.log(0 || 'Манго') // Манго
+
+// console.log(0 || 'false' || null)
+ 
+// || - превый true
+// && - первый false
+
+// console.log('Mango' && null || false);
+// 1) Mango && null // null
+// 2) null ||  // false
+
+// if (null) { 
+// console.log("Hello")
+// } else {
+//     console.log("World")
+// }
+
+
+
+
 // Логические операторы
 // // Что выведет код?
 
@@ -23,15 +69,83 @@
 // // Используя конструкцию if..else, напишите код, который будет спрашивать: «Какое количество месяцев в году». Если пользователь вводит 12 , 
 // то показать: «Верно!», в противном случае – отобразить: «Не знаете ? 12!»
 
+// 1) Попросить у юзера число
+// 2) Проверить равно ли оно 12
+// 3) На основе проверки запустить if или else
+
+// const userNumber = Number(prompt('Enter number'));
+
+// if (userNumber === 12) {
+//     console.log('Correct')
+// } else {
+//     console.log('Error')
+// }
+
+
 // // Написать программу которая получит от пользователя число и определит четное это число или не четное.
+
+// 1) Получить число
+// 2) Написать проверку на чет нечет
+// 3) Вивести результат
+
+
+// const day = prompt("Enter day");
+
+// if (day % 2 === 0) {
+//     console.log('Четное')
+// } else {
+//     console.log("Нечет")
+// }
+    
 
 // Написать программу где мы спрашиваем у пользователя номер месяца  и показываем к какому времени года он принадлежит 
 // Если пользователь вводит 2 - показываем слово Зима
+
+// 1) Получить данные от юзера
+// 2) Проверка на время года
+// 3) Вывести результат
+
+// const monthNumber = Number(prompt('enter month number'));
+
+// if (monthNumber === 1) {
+//     console.log('Зима')
+// } else if (monthNumber === 2) {
+//     console.log('Зима')
+// } else if (monthNumber === 12) {
+//     console.log('Зима')
+// }
+
+
+// if (monthNumber === 12 || monthNumber === 1 || monthNumber === 2) {
+//     console.log("Зима")
+// } else if (monthNumber >= 3 && monthNumber <= 5) {
+//     console.log('Весна');
+// } else if (monthNumber >= 6 && monthNumber <= 8) {
+//     console.log('Лето')
+// } else if (monthNumber >= 9 && monthNumber <= 11) {
+//     console.log('Осень')
+// } else {
+//     console.log('данные не валидны')
+// }
 
 // Написать программу которая проверяет логин и пароль пользователя и выводит приветствие - если все отлично и ошибку если неверный логин
 
 // const login = 'user@gmail.com';
 // const password = '12345'
+
+// const userLogin = prompt('Enter login')
+// const userPassword = prompt('Enter password')
+
+// if (login === userLogin && password === userPassword) {
+//     console.log('Wellcome')
+// } else {
+//     console.log('Error')
+// }
+
+// const result = login === userLogin && password === userPassword ? "Wellcome" : 'Error'
+
+// console.log('result', result)
+
 
 // написать програму которая спращивает у пользователя период подписки на сервис и показывает цену подписки
 // сначала if else потом switch 
@@ -39,6 +153,37 @@
 // Если Неделя - цена 5.99
 // Если месяц - цена 10.99
 // Если год - цена 100.99
+
+// const period = prompt('Enter period')
+
+// if (period === 'День') {
+//     console.log('1.99')
+// } else if (period === 'Неделя') {
+//     console.log('5.99')
+// } else if (period === 'Месяц') {
+//     console.log('10.99')
+// } else if (period === 'Год') {
+//     console.log('100.99')
+// }
+
+// switch (period) {
+//     case "День":
+//         console.log('1.99')
+//         break;
+//         case 'Неделя':
+//         console.log('5.99')
+//         break;
+//         case 'Месяц':
+//         console.log('10.99')
+//         break
+//         case 'Год':
+//         console.log('100.99')
+//         break
+//     default:
+//         console.log('Error')
+//         break;
+// }
+
 
 // Тернарный оператор
 // // Перепишите if...else с использованием тернарного оператора.
@@ -63,8 +208,48 @@
 
 // Напишите цикл for который выводит в консоль браузера числа по возрастанию от min до max.
 
-// const max = 100;
-// const min = 20;
+const max = 100;
+const min = 20;
+
+// for (let i = min; i <= max; i += 1) {
+//     console.log(i)
+// }
+
+// вывести только числа кратные 10
+
+// for (let i = min; i <= max; i += 10) {
+//     console.log(i)
+// } // 10 итераций
+
+// for (let i = min; i <= max; i += 1) {
+//     if (i % 10 === 0) {
+//         console.log(i) // 20  30
+//     }
+//     continue
+// }
+
+// for (let i = min; i <= max; i += 1) {
+//     if (i % 10 !== 0) {
+//         continue
+
+//     }
+//     console.log(i) // 20  30
+// }
+
+// for (let i = min; i <= max; i += 10) {
+//     if (i === 50) {
+//         break
+//     }
+//     console.log(i)
+// } // 10 итераций
+
+// for (let i = min; i < 50; i += 10) {
+    
+//     console.log(i)
+// } // 10 итераций
+
+
+
 
 // for(let i = min; i <= max; i += 1) {
 //   console.log(i)
@@ -102,13 +287,23 @@
 //     console.log(i);
 //   }
 
-//do do while
+//while do while
 
 // Напишите цикл, который предлагает ввести число большее 100 через prompt. 
 // Если посетитель ввёл другое число - попросить ввести ещё раз, и так далее.
 // Цикл должен спрашивать число пока посетитель не введёт число, большее 100, либо не нажмёт кнопку Отмена в промпт.
 
-// let number = 0;
+let number = 0;
+
+// while (number !== null && number <= 100) {
+//     number = prompt('Enter number');
+//     console.log("number", number)
+// }
+
+// do {
+//     number = prompt('Enter number');
+//     console.log("number", number);
+// } while (number !== null && number <= 100);
 
 // while (number !== null && number <= 100) {
 //     number = prompt('Enter number');
