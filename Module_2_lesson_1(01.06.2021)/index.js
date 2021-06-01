587e1eb57b3cf53c53f341fb91e9787cd41ff3d7
Vova Melnyk <hagmen4339@gmail.com>
2021-06-01 22:02:53 +0300

    add lesson 1

--- Module_2_lesson_1(01.06.2021)/index.js
+++ Module_2_lesson_1(01.06.2021)/index.js
@@ -1,13 +1,16 @@
 // 1) В чем разница ?
-// const clients = ['Mango', 'Poly', 'Ajax'];
+// const clients = ["Mango", "Poly", "Ajax"];
 
-// // Значение элемента можно заменить
-// clients[0] = 'Kiwi';
+// Значение элемента можно заменить
+// clients[0] = "Kiwi";
 // console.log(clients[0]); // Kiwi
 
-// // Или добавить
-// clients[3] = 'Alex';
-// console.log(clients[3]); // Alex
+// Или добавить
+// clients[5] = "Alex";
+// console.log(clients[3]);
+// console.log(clients[4]);
+// console.log(clients[5]);
+// clients[100] = "Bob";
 
 // console.log(clients); // ["Kiwi", "Poly", "Ajax", "Alex"]
 
@@ -26,15 +29,46 @@
 
 // 3) Можно ли в значения массива вставлять переменные ?
 // const newClients = ["Monkong", "Singu"];
+// const userName = "Bob";
 // const oldClients = ["Mango", "Ajax", "Poly", "Kiwi", newClients];
 // const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"].concat(newClients);
-// const oldClients = ["Mango", "Ajax", "Poly", "Kiwi", ...newClients];
+// const oldClients = ["Mango", "Ajax", ...newClients, "Poly", "Kiwi"];
 // console.log(oldClients);
+// const number = ["Bob"];
+// const number2 = [2];
+// console.log(number + number2);
 
 // 4) const для масивов
 // const clients = ["Mango", "Poly", "Ajax"];
 // clients = 5;
 // console.log(clients);
+
+// 5)  И чем отличается splice от переопределения?
+
+// const arr = [2, 'a', true, null];
+
+// const aar = [3, 'w']
+// const a = [true, null]
+
+// console.log([1, 3, 5] === [1, 3, 5]);
+
+// const a = 5;
+// const b = 5;
+
+// console.log(a === b);
+
+// const arr1 = [1, 2, 3]; // link1
+// const arr2 = [1, 2, 3]; // link2
+
+// console.log(arr1 === arr2);
+
+// const numbers = [7, 3, 8, 9, 2]; // link1
+// // const newNumbers = numbers.slice(3);
+// const newNumbers = numbers.slice(); // link2
+// console.log("numbers", numbers);
+// console.log(`newNumbers`, newNumbers);
+// numbers.push(4, 5, 6);
+// console.log(numbers);
 
 // 1) базовые операции с массивом
 // Создайте массив styles с элементами «Джаз» и «Блюз».
@@ -50,27 +84,39 @@
 // Классика, Рок-н-ролл
 // Рэп, Регги, Классика, Рок-н-ролл
 
-//  const roles = ["user", "tester", "admin"];
+// const styles = ["Джаз", "Блюз"];
+// styles.push("Рок-н-ролл");
+// styles.splice(1, 1, "Классика");
+// const deletedStyle = styles.shift();
+// styles.unshift("Рэп", "Регги");
+// console.log(deletedStyle);
+// console.log(styles);
+
+const roles = ["user", "tester", "admin"];
 // console.log(roles[1]);
 // console.log(roles[0]);
 // console.log(roles[5]);
 // console.log(roles.length);
 // console.log(roles[-1]);
-// console.log(roles[roles.length - 1]);
+// const lastIndex = roles.length - 1;
+// console.log(roles[lastIndex]);
 // roles.pop();
 // console.log(roles);
-// roles.push("support");
+// const result = roles.push("support");
 // console.log(roles);
 // roles.shift();
 // console.log(roles);
 // roles.unshift("manager");
 // console.log(roles);
 // roles.splice(2, 3);
+// console.log(roles);
 // roles.splice(2, 1, "user2");
 // roles.splice(2, 0, "user2");
 // console.log(roles);
 // const newArr = roles.slice();
-// console.log(roles.includes('tester'));
+// console.log("newArr", newArr);
+// console.log(roles === newArr);
+// console.log(roles.includes("tester"));
 
 // 2) Напиши функцию logItems(array), которая получает массив и использует цикл for, который для каждого элемента массива
 //будет выводить в консоль сообщение в формате < номер элемента > - <значение элемента >.
@@ -78,7 +124,37 @@
 // Например для первого элемента массива['Mango', 'Poly', 'Ajax'] с индексом 0 будет выведено
 // 1 - Mango, а для индекса 2 выведет 3 - Ajax.
 
+// const users = ["Mango", "Poly", "Ajax"]; // 3
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log("index", i); // 0 // 1 // 2
+//   console.log("value", users[i]); // users[0] // users[1] // users[2]
+// }
+
+// for (let user of users) {
+//   console.log("index", users.indexOf(user));
+//   console.log(user); // Mango //Poly  // Ajax
+// }
+
 // 3) Напиши код который ищет самое маленькое число в массиве.
+
+// const numbers = [2, 17, 94, 1, 23, 37];
+
+// 1) Определить стартовое минимальное число
+// 2) Сравнить это число со всеми последующими елементами масива
+// 3) Если стартовое число меньше числа с которым сравниваем - оставляем как есть
+// 4) Если стартовое число больше числа с которым срасниваем - пререзаписываем минимальное число
+
+// let smallestNumber = numbers[0]; // 2
+
+// for (let number of numbers) {
+//   // 2 // 17 // 94 // 1 // 23 // 37
+//   if (smallestNumber > number) {
+//     smallestNumber = number; // 1
+//   }
+// }
+
+// console.log(smallestNumber); // 1
 
 // const findSmallestNumber = function (numbers) {
 //   let smallestNumber = numbers[0];
@@ -97,7 +173,22 @@
 
 // 4) Проверить является ли строка палиндромом
 
-// const str = "довод";
+const str = "довод";
+const newString = "Hello";
+
+// 1) Сделать со строки масив
+// 2) Перевевнуть масив наоборот
+// 3) С масива сделать строку
+// 4) Сравнить 2 строки
+
+const array = str.split("");
+array.reverse();
+const reverseString = array.join("");
+console.log(reverseString);
+
+const reverseStringWithChaining = newString.split("").reverse().join("");
+
+console.log(newString === reverseStringWithChaining);
 
 // const strSplit = str.split("").reverse().join("");
 // console.log(strSplit);
@@ -111,3 +202,5 @@
 
 // 6) Написать програму которая спрашивает у пользователя цены, он вписывает их через prompt
 // пока не нажмет отмену. После этого посчитать суму чисел и используя alert вывести результат на экран
+
+// includes
