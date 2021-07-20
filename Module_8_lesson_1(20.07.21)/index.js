cc5fc5250dac32093c0493900b3aec45d71a9e17
Vova Melnyk <hagmen4339@gmail.com>
2021-07-20 21:52:06 +0300

    add lesson 1 module 8

--- Module_8_lesson_1(20.07.21)/index.js
+++ Module_8_lesson_1(20.07.21)/index.js
@@ -1,9 +1,10 @@
 //event.preventDefault();
 // const form = document.querySelector("form");
 // const input = document.querySelector("input");
+// const link = document.querySelector("a");
 // function onSubmit(e) {
 //   e.preventDefault();
-//   console.log(input.value);
+//   console.log("send data to server", input.value);
 // }
 
 // form.addEventListener("submit", onSubmit);
@@ -11,10 +12,10 @@
 // const [decrementBtn, num, incrementBtn] =
 //   document.querySelector("#counter").children;
 // const onIncrementClick = () => {
-//   num.textContent += 1;
+//   num.textContent++; // typeof
 // };
 // const onDecrementClick = () => {
-//   num.textContent -= 1;
+//   num.textContent--;
 // };
 
 // incrementBtn.addEventListener("click", onIncrementClick);
@@ -26,18 +27,81 @@
 //   }
 // }
 
-// window.addEventListener("keypress", enterHandler);
+// function enterHandler(e) {
+//   //   if (e.key !== "Enter") {
+//   //     return;
+//   //   }
+
+//   console.log("key", e);
+
+//   //   console.log(e.target.textContent);
+// }
+
+// window.addEventListener("keydown", enterHandler);
 
 // const parent = document.querySelector(".parent");
+// const modal = document.querySelector(".overlay");
+// const closeIcon = document.querySelector("#close");
 
 // function check(e) {
-//   console.log(e.target);
-//   console.log(e.currentTarget);
+// //   e.stopImmediatePropagation();
+//   console.log("target", e.target);
+//   console.log("currentTarget", e.currentTarget);
+//   //   console.log(e.target.dataset.status);
+//   //   console.log(e.target.dataset.value);
+//   //   console.log(e.target.dataset.number);
+
+//   //   console.log(parent.dataset);
 // }
 
 // parent.addEventListener("click", check);
 
+// function openModal(e) {
+//   modal.classList.add("open-modal");
+// }
+
+// function closeModal() {
+//   modal.classList.remove("open-modal");
+// }
+
+// function closeModalESC(e) {
+//   if (e.key !== "Escape") {
+//     return;
+//   }
+
+//   toggleModal();
+// }
+
+// function toggleModal() {
+//   modal.classList.toggle("open-modal");
+// }
+
+// parent.addEventListener("click", toggleModal);
+// closeIcon.addEventListener("click", toggleModal);
+// window.addEventListener("keyup", closeModalESC);
+
+// !e.key === escape?.return;
+
 // Сделать фиксированый скрол на старнице для менюшки
+
+// 1) Посчитать висоту хедера
+// 2) Отслеживаем позицию скролла
+// 2) Добавлять клас если скрол больше висоту хедера
+
+// const header = document.querySelector(".header");
+// const nav = document.querySelector(".nav");
+// const headerHeight = header.clientHeight;
+// pageYOffset;
+
+// function navbarFixed() {
+//   if (pageYOffset > headerHeight) {
+//     nav.classList.add("fixed-nav");
+//   } else {
+//     nav.classList.remove("fixed-nav");
+//   }
+// }
+
+// window.addEventListener("scroll", navbarFixed);
 
 // let header = document.querySelector(".header");
 // let nav = document.querySelector(".nav");
@@ -64,6 +128,4 @@
 // }
 // container.addEventListener("mouseover", changeCords);
 
-
 // event delegation
-
