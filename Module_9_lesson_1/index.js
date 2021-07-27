6ad2c89b056d89ce1d787fb65f206bd515cad8e4
Vova Melnyk <hagmen4339@gmail.com>
2021-07-27 17:35:58 +0300

    add module 9 lesson 1

---
+++ Module_9_lesson_1/index.js
@@ -0,0 +1,18 @@
+// ls
+// cd
+// mkdir test
+// pwd
+// rmdir
+// rm
+// clear
+// node -v
+// touch .gitignore;
+
+// run js with node
+// git ignore
+
+// versioning
+// 1.0.0
+// 1.0.1
+// 1.1.0
+// 2.0.0
