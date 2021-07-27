5984cc48364f3b94ee9caad308d5fd18870e0758
Vova Melnyk <hagmen4339@gmail.com>
2021-07-27 19:23:37 +0300

    add module_9_lesson 1

--- Module_9_lesson_1/index.js
+++ Module_9_lesson_1/index.js
@@ -10,9 +10,34 @@
 
 // run js with node
 // git ignore
+// node scripts
 
 // versioning
 // 1.0.0
 // 1.0.1
 // 1.1.0
 // 2.0.0
+
+// -P
+
+// -D
+
+// console.log("Hello");
+
+// nanoid
+// const { nanoid } = require("nanoid");
+// import { nanoid } from "nanoid";
+
+// console.log(nanoid());
+//lodash
+// const _ = require("lodash");
+// console.log(_.debounce);
+
+// isurl
+
+// import
+// export
+
+// import { sum } from "./test.js";
+
+// console.log(sum());
---
+++ Module_9_lesson_1/test.js
@@ -0,0 +1,1 @@
+export const sum = () => 2 + 3;
